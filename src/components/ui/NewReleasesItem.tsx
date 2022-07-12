6e5b2611d26781e69bc3b71e9690801ef8d87f12
franc <onyishif@gmail.com>
2022-07-12 03:56:52 +0100

    fetch favourites and display

--- src/components/ui/NewReleasesItem.tsx
+++ src/components/ui/NewReleasesItem.tsx
@@ -2,7 +2,7 @@ import { NewRelease } from '../../types/songs.types'
 
 function NewReleasesItem ({ id, images, name, album_type, total_tracks, release_date, selectAlbum }: NewRelease & { selectAlbum: React.Dispatch<React.SetStateAction<Pick<NewRelease, 'id'|'images'|'name'|'release_date' >| undefined>>}) {
   return (
-    <div className={`relative rounded-2xl w-64 h-56 flex-shrink-0 cursor-pointer hover:border hover:border-spoteefy-green`} style={{ backgroundImage: `url('${images[1].url}')` }} onClick={() => { selectAlbum({ id, images, release_date, name }) }}>
+    <div className='relative rounded-2xl w-64 h-56 flex-shrink-0 cursor-pointer hover:border hover:border-spoteefy-green' style={{ backgroundImage: `url('${images[1].url}')` }} onClick={() => { selectAlbum({ id, images, release_date, name }) }}>
       <div className='absolute bottom-0 bg-[#646165]/80 px-5 py-3 flex flex-col w-full rounded-b-2xl'>
         <div className='font-Raleway text-base text-white/90 font-semibold'>{name}</div>
         <div className='text-sm text-white/70 font-medium'>{`${total_tracks.toString()} ${album_type === 'album' ? 'new songs' : 'new song'}`}</div>
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,14 +1,32 @@
 import Navbar from '../components/ui/Navbar'
 import libraryImage from '../assets/images/libraryImage.jpg'
-import { useAppSelector } from '../types/hook.type'
+import SongItem from '../components/ui/SongItem'
+import { useAppDispatch, useAppSelector } from '../types/hook.type'
 import { AppRequest } from '../libs'
+import { useEffect, useState } from 'react'
+import { Song } from '../types/songs.types'
+import { setUserLibrary } from '../store/user'
 
 function Library (): JSX.Element {
   const token = useAppSelector((state) => state.user.token)
+  const myLibrary = useAppSelector((state) => state.user.library)
+  const dispatch = useAppDispatch()
+  const [myLibrarySongs, setMyLibrarySongs] = useState<Song[]>()
+  useEffect(() => {
+    if (myLibrary.length < 1) {
+      dispatch(setUserLibrary())
+    }
+    getLibrarySongInfo().then((data) => {
+      setMyLibrarySongs(data)
+    })
+  }, [myLibrary])
   const getLibrarySongInfo = async () => {
     if (token) {
-      const { items } = (await AppRequest('', token)).albums
-      return items
+      if (myLibrary.length < 1) {
+        return []
+      }
+      const { tracks } = (await AppRequest(`/tracks/?ids=${myLibrary.join(',')}`, token))
+      return tracks as Song[]
     }
   }
   return (
@@ -25,6 +43,12 @@ function Library (): JSX.Element {
         <div className='font-bold text-white/90 text-4xl sm:text-8xl font-Swash'>Music is Life itself</div>
       </div>
       <div className='font-Raleway font-bold text-2xl text-white/80 mt-8'>Your favourites ❤️‍🔥</div>
+      <div className='flex flex-col'>
+        {myLibrarySongs?.map((songItem, index) =>
+          songItem &&
+            <SongItem index={index} {...songItem} key={songItem.id} favourite />
+        )}
+      </div>
     </div>
   )
 }
